3635a861b3565881dae3358aa178a16e681d5f16
rebol42 <skati33@gmail.com>
2021-11-03 23:18:48 +0100

    add FizzBuzz

---
+++ FizzBuzz/GameFizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class GameFizzBuzz
+    {
+        public string FizzBuzz(int value)
+        {
+            string result = "";
+            int checkMod3;
+            int checkMod5;
+
+            checkMod3 = value % 3;
+            checkMod5 = value % 5;
+
+            if (checkMod3 == 0 && checkMod5 != 0)
+            {
+                result =  "Fizz";
+            }
+            if(checkMod5 == 0 && checkMod3 != 0)
+            {
+                result =  "Buzz";
+            }
+            if (checkMod3 == 0 && checkMod5 == 0)
+                result = "FizzBuzz";
+
+            if (checkMod3 != 0 && checkMod5 != 0)
+                result = Convert.ToString(value);
+
+            return result;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = 0;
+            GameFizzBuzz FizzBuzz = new GameFizzBuzz();
+
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę");
+
+                var userValue = getValue();
+    
+                Console.WriteLine(FizzBuzz.FizzBuzz(userValue));
+               
+            }
+
+        }
+        private static int getValue()
+        {
+            while (true)
+            {
+                var val = Console.ReadLine();
+
+                if (val.ToUpper() == "T")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(val, out int value))
+                {
+                    Console.WriteLine("Podana Wartość jest nieprawidłowa, spróbuj ponownie");
+                    continue;
+                }
+
+                return value;
+
+            }
+        }
+    }
+}
